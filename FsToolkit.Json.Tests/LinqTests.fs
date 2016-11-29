c684062d0dee7a830c741634df484b68bc853413
Stephen Swensen <stephen.swensen@relayfoods.com>
2016-11-28 20:52:52 -0500

    good progress on ? Json 'linq' operator

---
+++ FsToolkit.Json.Tests/LinqTests.fs
@@ -0,0 +1,79 @@
+﻿namespace FsToolkit.Json.Tests
+
+open System
+open Swensen.Unquote
+open NUnit.Framework
+open Newtonsoft.Json.Linq
+open FsToolkit.Json.Linq
+
+module LinqTests =
+    
+    [<Test>]
+    let ``get existing value type property`` () =
+        let json = JObject.Parse """{ hello: 3 }"""
+        test <@ json?hello = 3 @>
+
+    [<Test>]
+    let ``get existing value type property of option type`` () =
+        let json = JObject.Parse """{ hello: 3 }"""
+        test <@ json?hello = Some(3) @>
+
+    [<Test>]
+    let ``get missing value type property`` () =
+        let json = JObject.Parse """{ }"""
+        test <@ json?hello = 0 @>
+
+    [<Test>]
+    let ``get missing value type property of option type`` () =
+        let json = JObject.Parse """{ }"""
+        test <@ json?hello = (None:int option) @>
+        
+    [<Test>]
+    let ``missing property chain`` () =
+        let json = JObject.Parse """{ hello: {} }"""
+        test <@ json?hello?world?goodbye = 0 @>
+
+    [<Test>]
+    let ``missing property chain of option`` () =
+        let json = JObject.Parse """{ hello: {} }"""
+        test <@ json?hello?world?goodbye = (None:int option) @>
+
+    [<Test>]
+    let ``get value in property`` () =
+        let json = JObject.Parse """{ hello: { world: 3 } }"""
+        test <@ json?hello?world = 3 @>
+
+    [<Test>]
+    let ``guid conversion`` () =
+        let guid = Guid.NewGuid()
+        let json = JObject.Parse <| """{ hello: '""" + guid.ToString() + """' }"""
+        test <@ json?hello = guid @>
+
+    [<Test>]
+    let ``case tolerance`` () =
+        let json = JObject.Parse """{ hello: { world: 3 } }"""
+        test <@ json?Hello?WORLD = 3 @>
+
+    type Test1 = { Prop1: string; Prop2: int }
+
+    [<Test>]
+    let ``convert record type`` () =
+        let json = JObject.Parse """{ hello: { Prop1: "a", Prop2: 3 } }"""
+        test <@ json?hello = { Prop1="a"; Prop2=3 } @>
+
+    [<AllowNullLiteral>]
+    type Test2() =
+        member val Prop1 = "" with get, set
+        member val Prop2 = 0 with get, set
+
+    [<Test>]
+    let ``convert regular type`` () =
+        let json = JObject.Parse """{ hello: { Prop1: "a", Prop2: 3 } }"""
+        let actual: Test2 = json?hello
+        test <@ actual.Prop1 = "a" && actual.Prop2 = 3 @>
+
+    [<Test>]
+    let ``convert regular type when null`` () =
+        let json = JObject.Parse """{ hello: null }"""
+        let actual: Test2 = json?hello
+        test <@ actual = null @>
---
+++ FsToolkit.Json/Linq.fs
@@ -0,0 +1,52 @@
+﻿namespace FsToolkit.Json
+
+open System
+open Newtonsoft.Json.Linq
+open Microsoft.FSharp.Reflection
+open System.Reflection
+
+module Linq =
+
+    ///True if missing or null valued property
+    let isNullToken (j: JObject) = 
+        isNull j || j.Type = JTokenType.Null
+
+    ///None if 't is a type of Option<_>, null if 't is a reference type, default if 't is a value type
+    let inline nullTokenObject<'t> =
+        if typedefof<'t> = typedefof<FSharp.Core.Option<_>> then
+            let cases = FSharpType.GetUnionCases (typeof<'t>)
+            FSharpValue.MakeUnion (cases.[0], [||]) :?> 't
+        else
+            Unchecked.defaultof<'t>
+
+    ///Case tolerant get property value (exact case is attempted first)
+    let getPropertyValue (j:JObject) (n:string) =
+        let jn' = j.[n]
+        if jn' = null then
+            j.Properties()
+            |> Seq.tryFind (fun p -> String.Equals(p.Name, n, StringComparison.OrdinalIgnoreCase))
+            |> (function None -> null | Some(p) -> p.Value)
+        else
+            jn'
+
+    ///Null and case tolerant dyanmic JObject selection with support for
+    ///option type wrappers around results.
+    let inline (?) (j : JObject) (n : string) : 't =
+        if isNullToken j then
+            nullTokenObject<'t>
+        else
+            let jn = getPropertyValue j n
+            if isNull jn || jn.Type = JTokenType.Null then
+                nullTokenObject<'t>
+            else
+                if typedefof<'t> = typedefof<FSharp.Core.Option<_>> then
+                    let cases = FSharpType.GetUnionCases (typeof<'t>)
+                    let args = typeof<'t>.GetGenericArguments ()
+                    let optionOf =
+                        match args.[0].IsValueType with
+                        | true -> (typedefof<Nullable<_>>).MakeGenericType ([| args.[0] |])
+                        | _ -> args.[0]
+                    let value = jn.ToObject(optionOf)
+                    FSharpValue.MakeUnion (cases.[1], [| value |]) :?> 't
+                else
+                    jn.ToObject<'t>()
