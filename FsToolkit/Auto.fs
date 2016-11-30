53beb0d26d3166ee1805a5df4a44635f6891e73f
Stephen Swensen <stephen.swensen@relayfoods.com>
2016-11-30 07:00:19 -0500

    Add core FsToolkit project w/ nuspec and simple Auto impls

---
+++ FsToolkit/Auto.fs
@@ -0,0 +1,8 @@
+﻿namespace FsToolkit
+
+[<AutoOpen>]
+module Auto = 
+    ///Coalesce option value
+    let inline (|?) x y = defaultArg x y
+    ///Coalesce null value
+    let inline (|??) (x:'a) y = if obj.ReferenceEquals(x, Unchecked.defaultof<'a>) then y else x
