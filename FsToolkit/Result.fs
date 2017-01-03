03c5b5a5167786716ddcd19402500e99db7dd8d2
Stephen Swensen <stephen.swensen@gmail.com>
2017-01-03 13:44:34 -0500

    redefine Result type to match definition coming out of the box in F# 4.1 for forward compatibility

--- FsToolkit/Result.fs
+++ FsToolkit/Result.fs
@@ -1,44 +1,47 @@
 ﻿namespace FsToolkit
 
+//Matches definition from https://github.com/Microsoft/visualfsharp/pull/964
 ///Type for Railway Oriented Programming: http://fsharpforfunandprofit.com/posts/recipe-part2/
-type Result<'TSuccess,'TFailure> =
-    | Success of 'TSuccess
-    | Failure of 'TFailure
+[<StructuralEquality; StructuralComparison>]
+[<CompiledName("FSharpResult`2")>]
+type Result<'T,'TError> = 
+    | Ok of 'T 
+    | Error of 'TError
 
 module Result =
     let bind f r =
         match r with
-        | Success x -> f(x)
-        | Failure x -> Failure x
+        | Ok x -> f(x)
+        | Error x -> Error x
     let (>>=) r f = bind f r
 
     let switch f x = 
-        f x |> Success
+        f x |> Ok
 
     let map f r =
         match r with
-        | Success x -> Success(f x)
-        | Failure x -> Failure x
+        | Ok x -> Ok(f x)
+        | Error x -> Error x
 
 module AsyncResult =
     let bind f r = async {
         let! r = r
         match r with
-        | Success x -> return! f(x)
-        | Failure x -> return Failure x
+        | Ok x -> return! f(x)
+        | Error x -> return Error x
     }
     let (>>=) r f = bind f r
 
     let map f r = async {
         let! r = r
         match r with
-        | Success x -> 
+        | Ok x -> 
             let! x' = f x
-            return Success(x')
-        | Failure x -> return Failure x
+            return Ok(x')
+        | Error x -> return Error x
     }
 
     let switch f x = async {
         let! x = f x
-        return x |> Success
+        return x |> Ok
     }
