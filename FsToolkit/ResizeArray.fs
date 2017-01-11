46241307b5fd77e8bf8a246ff73b2913a3a6d838
Stephen Swensen <stephen.swensen@relayfoods.com>
2017-01-11 05:06:02 -0500

    add ResizeArray extension module

---
+++ FsToolkit/ResizeArray.fs
@@ -0,0 +1,18 @@
+﻿namespace FsToolkit
+
+open System
+open System.Collections.Generic
+
+module ResizeArray =
+    let remove predicate (xs: ResizeArray<'t>) =
+        let i = xs.FindIndex(Predicate predicate)
+        xs.RemoveAt i
+
+    let replace predicate item (xs: ResizeArray<'t>) =
+        let i = xs.FindIndex(Predicate predicate)
+        xs.[i] <- item
+
+    let replaceOrAdd predicate item (xs: ResizeArray<'t>) =
+        match xs.FindIndex(Predicate predicate) with
+        | -1 -> xs.Add(item)
+        | i  -> xs.[i] <- item
