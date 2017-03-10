47dbe25309931ff97240c35339a26a9dc13e4a76
Stephen Swensen <stephen.swensen@relayfoods.com>
2017-03-10 11:38:06 -0500

    add nuspec for http project

---
+++ FsToolkit.Http/Scripts/script.fsx
@@ -0,0 +1,10 @@
+﻿#I __SOURCE_DIRECTORY__
+#load "load-references-debug.fsx"
+#load "../AssemblyInfo.fs"
+      "../FastHttp.fs"
+
+open System
+open FsToolkit.Http
+open FsToolkit.Http.FastHttp
+let request = { FastRequest.Default with Method="POST"; Body="hello"; Headers=["content-type", "application/json; charset=utf-8"] }
+send request
