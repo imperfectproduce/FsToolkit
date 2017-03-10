a4d982d9990ed7fe9ac320837c62c55a24f2b25c
Stephen Swensen <stephen.swensen@relayfoods.com>
2017-03-10 09:54:24 -0500

    encoding work

--- FsToolkit.Http/FastHttp.fs
+++ FsToolkit.Http/FastHttp.fs
@@ -5,6 +5,11 @@ open System.Net
 open System.Net.Http
 open System.Net.Http.Headers
 open System.Threading
+open System.Text
+open System.Text.RegularExpressions
+
+//TODO
+// - query params
 
 type FastRequest = {
     Method : string
@@ -15,18 +20,21 @@ type FastRequest = {
     Timeout : TimeSpan
     ///The optional CancellationToken (Timeout is ignored if given)
     CancellationToken : CancellationToken option
+    ///Indicates whether or not UTF-8 BOM should be include when encoding / decoding utf-8 charset content
+    IncludeUtf8Bom : bool
 } with 
     ///Default fast request: GET | http://example.com | JSON | no body | timeout = 12s
     static member Default = {
         Method = "GET" 
         Url = "http://example.org"
         Headers = 
-            [ ("content-type", "application/json; charset=utf8") 
+            [ ("content-type", "application/json; charset=utf-8") 
               ("accept", "application/json, text/html, text/plain, application/xml, application/xhtml+xml") 
               ("Accept-Encoding", "gzip, deflate") ]
         Body = null
         Timeout = TimeSpan.FromSeconds(12.)
         CancellationToken = None
+        IncludeUtf8Bom = false
     }
 
 type FastResponse = {
@@ -38,17 +46,39 @@ type FastResponse = {
 module FastHttp =
     //don't limit number of http connections
     do System.Net.ServicePointManager.DefaultConnectionLimit <- Int32.MaxValue
+    do System.Net.ServicePointManager.Expect100Continue <- false
     //take advantage singleton built-in socket pooling
 
-    let handler = new HttpClientHandler()
+    let private handler = new HttpClientHandler()
     do
         handler.AllowAutoRedirect <- false
         handler.UseCookies <- false
         handler.UseDefaultCredentials <- true
-        handler.AutomaticDecompression <- handler.AutomaticDecompression ||| DecompressionMethods.GZip
-        handler.AutomaticDecompression <- handler.AutomaticDecompression ||| DecompressionMethods.Deflate
+        handler.AutomaticDecompression <- DecompressionMethods.GZip ||| DecompressionMethods.Deflate
 
-    let client = new HttpClient(handler)
+    let private client = new HttpClient(handler)
+    do client.DefaultRequestHeaders.ExpectContinue <- Nullable(false)
+
+    let private utf8Encoding includeBom = UTF8Encoding(includeBom) :> Encoding
+
+    let createRequestContent (fastRequest:FastRequest) =
+        let ct = fastRequest.Headers |> Seq.tryFind (fun (k,v) -> k.ToLower() = "content-type")
+        match ct with
+        | Some(_,ct) ->
+            let parts = System.Text.RegularExpressions.Regex.Match(ct, @"(?<mime>[^;]*)(;\s*charset=(?<charset>.*))?")
+            printfn "%A" parts
+            match parts.Groups.[2], parts.Groups.[3] with
+            | mime, charset when mime.Success && charset.Success ->
+                let encoding =
+                    match charset.Value with
+                    | "utf-8" ->  utf8Encoding fastRequest.IncludeUtf8Bom
+                    | charset -> Encoding.GetEncoding(charset)
+                new StringContent(fastRequest.Body, encoding, mime.Value)
+            | mime, charset when mime.Success && not charset.Success ->
+                new StringContent(fastRequest.Body, utf8Encoding fastRequest.IncludeUtf8Bom, mime.Value)
+            | _ -> failwith "Couldn't parse content-type header"
+        | None ->
+            new StringContent(fastRequest.Body, utf8Encoding fastRequest.IncludeUtf8Bom, "text/plain")
 
     let sendAsync fastRequest = async {
         use request = new HttpRequestMessage()
@@ -57,16 +87,10 @@ module FastHttp =
         for (k,v) in fastRequest.Headers do
             if ["content-type"] |> Seq.contains (k.ToLower()) |> not then
                 request.Headers.TryAddWithoutValidation(k,v) |> ignore
-        match fastRequest.Body with
-        | null | "" -> ()
-        | body -> 
-            request.Content <- new StringContent(body)
-            match fastRequest.Headers |> Seq.tryFind (fun (k,v) -> k.ToLower() = "content-type") with
-            | Some(_,value) ->
-                let value = value.Split(';').[0]
-                request.Content.Headers.ContentType <- new MediaTypeHeaderValue(value)
-            | None ->
-                request.Content.Headers.ContentType <- new MediaTypeHeaderValue("application/json")
+
+        if fastRequest.Body |> String.IsNullOrEmpty |> not && ["GET";"HEAD"] |> Seq.contains fastRequest.Method |> not
+        then request.Content <- createRequestContent fastRequest
+        else ()
 
         let ct = 
             match fastRequest.CancellationToken with
@@ -79,8 +103,7 @@ module FastHttp =
         let! responseBody = response.Content.ReadAsStringAsync() |> Async.AwaitTask
         let responseHeaders =
             response.Headers // does this include the content header?
-            |> Seq.map (|KeyValue|)
-            |> Seq.map (fun (k,v) -> k, String.Join(", ", v))
+            |> Seq.map (function KeyValue(k,v) -> k, String.Join(", ", v))
             |> Seq.toList
 
         return {
