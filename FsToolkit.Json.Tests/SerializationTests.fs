80ab7f7816b5a3f61bda10aaacc049f4d38d2c77
Stephen Swensen <stephen.swensen@relayfoods.com>
2017-06-09 22:13:36 -0400

    insertiondictionary test

--- FsToolkit.Json.Tests/SerializationTests.fs
+++ FsToolkit.Json.Tests/SerializationTests.fs
@@ -45,36 +45,12 @@ module SerializtionTests =
                 let value' = deserialize Client json
                 test <@ value = value' @>
 
-        //type InsertionDictionary<'T, 'K when 'T : equality>() = 
-        //    let dictionary = new ResizeArray<KeyValuePair<'T, 'K>>()
-
-        //    interface IDictionary<'T,'K> with
-        //        member this.Add(key, value) = dictionary.Add(KeyValuePair<'T,'K>(key, value))
-        //        member this.Add(kvp) = dictionary.Add(kvp)
-        //        member this.ContainsKey(key) = dictionary |> Seq.exists(fun kvp -> kvp.Key = key)
-        //        //member this.Contains(kvp) = dictionary.TryGetValue(kvp.Key.ToUpper(), ref kvp.Value)
-        //        //member this.Item with get key = dictionary.Item(key) and set key value = dictionary.Item(key) <- value
-        //        //member this.Count with get() = dictionary.Count
-        //        //member this.IsReadOnly with get() = false
-        //        //member this.Keys = dictionary.Keys :> ICollection<String>
-        //        //member this.Remove key = dictionary.Remove(key)
-        //        //member this.Remove(kvp : KeyValuePair<String,String>) = dictionary.Remove(kvp.Key.ToUpper())
-        //        //member this.TryGetValue(key, value) = dictionary.TryGetValue(key, ref value)
-        //        //member this.Values = dictionary.Values :> ICollection<String>
-        //        //member this.Clear() = dictionary.Clear()
-        //        //member this.CopyTo(array, arrayIndex) = (dictionary :> IDictionary<string, string>).CopyTo(array, arrayIndex)
-        //        //member this.GetEnumerator() = dictionary.GetEnumerator() :> System.Collections.IEnumerator
-        //        //member this.GetEnumerator() = dictionary.GetEnumerator() :> IEnumerator<KeyValuePair<String,String>>
-
         [<Test>]
         let ``map insertion order`` () =
             let json = """{ b: 1, a: 2 }"""
-            let value: System.Collections.Specialized.OrderedDictionary = deserialize Client json
-            let expected = [("b", 1L); ("a", 2L)]
-            let actual = 
-                let keys = seq { for k in value.Keys -> k :?> string }
-                let values = seq { for v in value.Values -> v :?> int64 }
-                Seq.zip keys values |> Seq.toList
+            let value: FsToolkit.InsertionDictionary<string,int> = deserialize Client json
+            let expected = [("b", 1); ("a", 2)]
+            let actual = value |> Seq.map (|KeyValue|) |> Seq.toList
             test <@ expected = actual @>
 
     module Storage =
