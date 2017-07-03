2f1cb068094446825dcc61a398a7c538dbc4050b
Stephen Swensen <stephen.swensen@relayfoods.com>
2017-07-03 12:05:10 -0400

    add some more ResizeArray extensions

--- FsToolkit/ResizeArray.fs
+++ FsToolkit/ResizeArray.fs
@@ -1,18 +1,35 @@
 ﻿namespace FsToolkit
 
 open System
-open System.Collections.Generic
 
 module ResizeArray =
+    ///Remove the item matching the predicate
     let remove predicate (xs: ResizeArray<'t>) =
         let i = xs.FindIndex(Predicate predicate)
         xs.RemoveAt i
 
+    ///Try remove the item matching the predicate, returning it if exists
+    let tryRemove predicate (xs: ResizeArray<'t>) =
+        match xs.FindIndex(Predicate predicate) with
+        | -1 -> None
+        | i ->
+            let item = xs.[i]
+            xs.RemoveAt i
+            Some(item)
+
+    ///Replace the item matching the predicate with the given replacement item
     let replace predicate item (xs: ResizeArray<'t>) =
         let i = xs.FindIndex(Predicate predicate)
         xs.[i] <- item
 
+    ///Replace or add (to the end) the item matching the predicate with the given replacement item
     let replaceOrAdd predicate item (xs: ResizeArray<'t>) =
         match xs.FindIndex(Predicate predicate) with
         | -1 -> xs.Add(item)
         | i  -> xs.[i] <- item
+
+    ///Replace the item matching the given predicate with the result of `f`
+    let update predicate f (xs:ResizeArray<_>) =
+        let i = xs.FindIndex(Predicate predicate)
+        let item = xs.[i]
+        xs.[i] <- f item
