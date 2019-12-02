12c6ae891b871f1a08586458754215baa0019ca2
Stephen Swensen <stephen@imperfectproduce.com>
2019-12-02 00:38:02 -0500

    add a new function that auto converts .net types to postgres types

--- FsToolkit.Postgres/PostgresAdo.fs
+++ FsToolkit.Postgres/PostgresAdo.fs
@@ -10,7 +10,44 @@ module PostgresAdo =
         let np = new NpgsqlParameter(name, ty)
         np.Value <- value :> obj
         np
-        
+
+    ///Build a query param with NpgsqlSqlDbType automatically detected and nulls and
+    ///option types automatically handled.
+    let P' (name:string, value: 'a) =
+        let value = value :> obj
+        match value with
+        //atoms
+        | :? string as v when v = null -> P(name, NpgsqlDbType.Text, DBNull.Value)
+        | :? string as v ->  P(name, NpgsqlDbType.Text, v)
+        | :? Guid as v -> P(name, NpgsqlDbType.Uuid, v)
+        | :? Int32 as v -> P(name, NpgsqlDbType.Integer, v)
+        | :? decimal as v -> P(name, NpgsqlDbType.Numeric, v)
+        | :? DateTime as v -> P(name, NpgsqlDbType.Timestamp, v)
+        | :? DateTimeOffset as v -> P(name, NpgsqlDbType.TimestampTz, v)
+        //None options
+        | :? option<string> as v when v.IsNone -> P(name, NpgsqlDbType.Text, DBNull.Value)
+        | :? option<Guid> as v when v.IsNone -> P(name, NpgsqlDbType.Uuid, DBNull.Value)
+        | :? option<Int32> as v when v.IsNone -> P(name, NpgsqlDbType.Integer, DBNull.Value)
+        | :? option<decimal> as v when v.IsNone -> P(name, NpgsqlDbType.Numeric, DBNull.Value)
+        | :? option<DateTime> as v when v.IsNone -> P(name, NpgsqlDbType.Timestamp, DBNull.Value)
+        | :? option<DateTimeOffset> as v when v.IsNone -> P(name, NpgsqlDbType.TimestampTz, DBNull.Value)
+        //Some options
+        | :? option<string> as v when v.Value = null -> P(name, NpgsqlDbType.Text, DBNull.Value)
+        | :? option<string> as v -> P(name, NpgsqlDbType.Text, v.Value)
+        | :? option<Guid> as v -> P(name, NpgsqlDbType.Uuid, v.Value)
+        | :? option<Int32> as v -> P(name, NpgsqlDbType.Integer, v.Value)
+        | :? option<decimal> as v -> P(name, NpgsqlDbType.Numeric, v.Value)
+        | :? option<DateTime> as v -> P(name, NpgsqlDbType.Timestamp, v.Value)
+        | :? option<DateTimeOffset> as v -> P(name, NpgsqlDbType.TimestampTz, v.Value)
+        //array-like
+        | :? seq<string> as v -> P(name, NpgsqlDbType.Array ||| NpgsqlDbType.Text, v |> Seq.toArray)
+        | :? seq<Guid> as v -> P(name, NpgsqlDbType.Array ||| NpgsqlDbType.Uuid, v |> Seq.toArray)
+        | :? seq<Int32> as v -> P(name, NpgsqlDbType.Array ||| NpgsqlDbType.Integer, v |> Seq.toArray)
+        | :? seq<decimal> as v -> P(name, NpgsqlDbType.Array ||| NpgsqlDbType.Numeric, v |> Seq.toArray)
+        | :? seq<DateTime> as v -> P(name, NpgsqlDbType.Array ||| NpgsqlDbType.Timestamp, v |> Seq.toArray)
+        | :? seq<DateTimeOffset> as v -> P(name, NpgsqlDbType.Array ||| NpgsqlDbType.TimestampTz, v |> Seq.toArray)
+        | v -> failwithf "Could not convert value to NpgsqlParameter: %A" v
+
     ///Execute a select query
     let execQuery (openConn:NpgsqlConnection) sql ps read =
         use cmd = openConn.CreateCommand()
