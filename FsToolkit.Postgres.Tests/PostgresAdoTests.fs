324fb3f6e83a05caa5888aa3c635004b0d33aedf
Stephen Swensen <stephen@imperfectproduce.com>
2020-10-14 05:51:55 -0400

    create Postgres.Tests project

---
+++ FsToolkit.Postgres.Tests/PostgresAdoTests.fs
@@ -0,0 +1,19 @@
+﻿namespace FsToolkit.Postgres.Tests
+
+open System
+open NpgsqlTypes
+open Swensen.Unquote
+open NUnit.Framework
+open Newtonsoft.Json.Linq
+open FsToolkit.Postgres
+
+module PostgresAdoTests =
+    
+    [<Test>]
+    let ``P' basic`` () =
+        let p = P'("x", 3)
+        test <@ p.ParameterName = "x" @>
+        test <@ p.NpgsqlValue = (3 :> obj) @>
+        test <@ p.NpgsqlDbType = NpgsqlDbType.Integer @>
+        
+        
