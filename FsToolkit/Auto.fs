afec4d9d3714ad1139f1889ced70874c57c9feaf
Stephen Swensen <stephen@imperfectproduce.com>
2020-11-24 10:33:04 -0500

    add reraisePreserve helper

--- FsToolkit/Auto.fs
+++ FsToolkit/Auto.fs
@@ -4,11 +4,19 @@ open System
 open System.Collections.Generic
 
 [<AutoOpen>]
-module Auto = 
+module Auto =
+    open System.Runtime.ExceptionServices
+
+    ///Re-raise the given exception, preserving stacktrace. This can within async computations
+    ///and other places where `reraise` can't be used. See https://stackoverflow.com/a/7169093
+    let inline reraisePreserve ex =
+        (ExceptionDispatchInfo.Capture ex).Throw ()
+        Unchecked.defaultof<_>
+
     let memoize (f: 'a -> 'b) =
         let cache = System.Collections.Concurrent.ConcurrentDictionary<'a, 'b>()
         fun x -> cache.GetOrAdd(x, f)
-        
+
     ///Coalesce option value
     let inline (|?) x y = defaultArg x y
     ///Coalesce null value
@@ -29,7 +37,7 @@ module Auto =
         let mutable o = Unchecked.defaultof<(^o)>
         if (^o : (static member TryParse : string * ^o byref -> bool) (s, &o)) then
             Some o
-        else 
+        else
             None
 
     let (|Int|_|) x = tryParse x : int option
@@ -38,11 +46,11 @@ module Auto =
     let (|Double|_|) x = tryParse x : Double option
     let (|Bool|_|) x = tryParse x : bool option
     let (|Decimal|_|) x = tryParse x : Decimal option
-    
-    let inline aprintfn fmt = 
+
+    let inline aprintfn fmt =
         Printf.ksprintf (Console.Out.WriteLineAsync>>Async.AwaitTask) fmt
 
-    module UTF8 = 
+    module UTF8 =
         ///String to bytes (utf-8 no BOM)
         let toBytes (text:string) = System.Text.UTF8Encoding(false).GetBytes(text)
         let toString (bytes:byte[]) = System.Text.UTF8Encoding(false).GetString(bytes)
@@ -54,7 +62,7 @@ module Auto =
         /// Trims a string, returning None if string is null or whitespace.
         let trimToOption str =
             if isEmpty str then None
-            else Some (str.Trim())  
+            else Some (str.Trim())
 
     type Uri with
         /// Gets the part of the URI before the last '/' character.
